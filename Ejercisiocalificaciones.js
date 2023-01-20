50f9aa5dbd22810c4c71babfa80804a52e81df48
Pablo Lucero <palucero@sudamericano.edu.ec>
2023-01-20 08:19:26 -0500

    first comit

---
+++ Ejercisiocalificaciones.js
@@ -0,0 +1,12 @@
+let ratings = [23,45,75,80]
+for(let i = 0; i<ratings.length; i++){
+
+        if(ratings[i]>=70)
+    {
+    console.log(ratings[i]+"aprueba")
+    }
+        else {
+         console.log(ratings[i]+"reprueba")
+    }
+
+}
---
+++ Ejercisiolimit3.js
@@ -0,0 +1,4 @@
+let num = parseInt(prompt("Ingrese numero:"));
+for (let i=1; i< num;i++){
+    console.log (i);
+}
